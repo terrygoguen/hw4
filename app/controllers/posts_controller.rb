663f49d83f8ddbd3384e588d2368c1dcca164ff8
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-03-08 19:00:10 +0000

    authenitcating to see user's own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.save
+    if @current_user
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = @current_user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@post["place_id"]}"
   end
 
