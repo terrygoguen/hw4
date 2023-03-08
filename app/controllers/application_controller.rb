05fc6b9f8b8d70dff421ab8e20f3a3eead7c9cfa
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-03-08 01:39:13 +0000

    added current user info and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    # puts "------------------ code before every request ------------------"
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
 end
